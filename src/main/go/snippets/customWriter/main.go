6b8b3758e69b67945366988ece7a0e880364c094
Piotr Wera <piotr.wera@vp.pl>
2019-10-13 18:24:57 +0200

    go customWriter

---
+++ src/main/go/snippets/customWriter/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type customWriter struct{}
+
+func (customWriter) Write(bs []byte) (int, error) {
+	fmt.Print(string(bs))
+	return len(bs), nil
+}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Printf("Err %v", err)
+	}
+	fmt.Println(resp)
+	io.Copy(customWriter{}, resp.Body)
+}
+
+func printMap(c map[string]string) {
+	for k, v := range c {
+		fmt.Printf("%v %v\n", k, v)
+	}
+}
