5518e707397b12a84a34ec58793674756226445d
pwera <piotr.wera@vp.pl>
2019-12-14 20:48:12 +0100

    go PrintStack

---
+++ src/main/go/snippets/stacktrace/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+func main() {
+	debug.PrintStack()
+	defer debug.PrintStack()
+	done := make(chan bool)
+
+	go func(done chan bool) {
+		debug.PrintStack()
+		done <- true
+		close(done)
+	}(done)
+	<-done
+
+	starckTrace := debug.Stack()
+	fmt.Printf("%v", string(starckTrace))
+	//os.Stdout.Write(debug.Stack())
+
+}
