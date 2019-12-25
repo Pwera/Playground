c0d3c1e95786fcf79bd9c73378c3fe4813895f30
pwera <piotr.wera@vp.pl>
2019-12-26 00:47:00 +0100

    go blockchain

---
+++ src/main/go/snippets/_new/blockchain/transaction.go
@@ -0,0 +1,39 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+)
+
+type Transaction struct {
+	Hash    []byte
+	Payload []byte
+}
+
+func NewTransaction(payload []byte) *Transaction {
+	tx := Transaction{Payload: payload}
+	tx.Hash, _ = GetTransactionHash(tx)
+
+	return &tx
+}
+
+func GetTransactionHash(tx Transaction) ([]byte, error) {
+	txBytes, err := tx.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	h := sha256.New()
+	h.Write(txBytes)
+
+	return h.Sum(nil), nil
+}
+
+func (tx *Transaction) MarshalBinary() ([]byte, error) {
+	var buf bytes.Buffer
+	err := binary.Write(&buf, binary.BigEndian, tx.Payload)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
